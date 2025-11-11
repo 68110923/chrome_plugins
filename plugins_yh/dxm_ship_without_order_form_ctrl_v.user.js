a1f44fdc320327e7f78dd67f5e21ad993e09a8ea
小鳄鱼 <68110923@qq.com>
2025-11-11 18:18:05 +0800

    第一个接口调通阶段提交

--- plugins_yh/dxm_ship_without_order_form_ctrl_v.user.js
+++ plugins_yh/dxm_ship_without_order_form_ctrl_v.user.js
@@ -9,8 +9,8 @@
 // @grant        unsafeWindow
 // @grant        GM_log
 // @grant        GM_notification
-// @downloadURL https://raw.githubusercontent.com/68110923/chrome_plugins/main/plugins_yh/dxm_ship_without_order_form_ctrl_v.js
-// @updateURL https://raw.githubusercontent.com/68110923/chrome_plugins/main/plugins_yh/dxm_ship_without_order_form_ctrl_v.js
+// @downloadURL https://raw.githubusercontent.com/68110923/chrome_plugins/main/plugins_yh/dxm_ship_without_order_form_ctrl_v.user.js
+// @updateURL https://raw.githubusercontent.com/68110923/chrome_plugins/main/plugins_yh/dxm_ship_without_order_form_ctrl_v.user.js
 // ==/UserScript==
 
 
