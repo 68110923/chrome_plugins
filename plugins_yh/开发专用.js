096f8f3c6db76c9d868022c8b60ce6cb7b4eaf8c
小鳄鱼 <68110923@qq.com>
2025-11-24 15:27:32 +0800

    第一个接口调通阶段提交

--- plugins_yh/开发专用.js
+++ plugins_yh/开发专用.js
@@ -14,6 +14,6 @@
 // @require      https://cdn.jsdelivr.net/npm/xlsx@0.18.5/dist/xlsx.full.min.js
 
 
-// @require      file:///C:\Users\admin\WebstormProjects\chrome_plugins\plugins_yh\shein_removed_shelves.user.js
+// @require      file:///C:\Users\admin\WebstormProjects\chrome_plugins\plugins_yh\shein_export_premium_pending_confirmation.user.js
 // ==/UserScript==
 console.log('主脚本已执行');
