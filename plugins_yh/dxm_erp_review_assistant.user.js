7ddcafd0c2a6988606002eff99b0c54dbdc6589d
小鳄鱼 <68110923@qq.com>
2026-01-13 11:49:11 +0800

    第一个接口调通阶段提交

--- plugins_yh/dxm_erp_review_assistant.user.js
+++ plugins_yh/dxm_erp_review_assistant.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         店小秘审单助手 - ERP版
 // @namespace    http://tampermonkey.net/
-// @version      1.0.9
+// @version      1.1.0
 // @description  1)店小秘自动添加初始备注, 2)Amazon商品数据提取, 3) TikTok商品数据提取, 4) 1688商品数据提取
 // @author       大大怪将军
 // @match        https://www.dianxiaomi.com/web/order/*
@@ -79,14 +79,16 @@
             });
 
             // https://detail.1688.com/offer/927463287246.html
+            // https://detail.1688.com/offer/858885203091.html
             if (skuList.length === 0) {
                 const allPropertyElements = document.querySelectorAll('.sku-props-list > .item-selected > .prop-item-text')
                 const allProperty = Array.from(allPropertyElements).map(skuElement => skuElement.textContent.trim());
-                const countElement = document.querySelector('.gyp-order-num-text');
-                const count = countElement ? countElement.textContent.trim().match(/[0-9.]+/)[0].trim() : 0;
-                if (allProperty && count) {
-                    skuList.push(`${allProperty.join('|')}*${count}`)
-                }
+                const countElements = document.querySelectorAll('.sku-list-item:has(input:not([value="0"]))');
+                skuList = Array.from(countElements).map(countElement => {
+                    const subSku = countElement.querySelector('.sku-item-name-text').getAttribute('title');
+                    const count = countElement.querySelector('input').getAttribute('value');
+                    return `${allProperty.join('|')}|${subSku}*${count}`
+                });
             }
 
             // https://detail.1688.com/offer/746259328464.html
