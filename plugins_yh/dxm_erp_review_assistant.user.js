7ff05cc1a26dcac05be4614273c28d953764b8c1
小鳄鱼 <68110923@qq.com>
2026-01-20 18:29:35 +0800

    第一个接口调通阶段提交

--- plugins_yh/dxm_erp_review_assistant.user.js
+++ plugins_yh/dxm_erp_review_assistant.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         店小秘审单助手 - ERP版
 // @namespace    http://tampermonkey.net/
-// @version      1.2.8
+// @version      1.2.9
 // @description  1)店小秘自动添加初始备注, 2)Amazon商品数据提取, 3) TikTok商品数据提取, 4) 1688商品数据提取
 // @author       大大怪将军
 // @match        https://www.dianxiaomi.com/web/order/*
@@ -89,11 +89,13 @@
         inputElement.value = href.match(/\/(\d+)\.html/)[1]
         inputElement.dispatchEvent(new Event('input', {bubbles: true, cancelable: true}));
         document.querySelector('#btnSelectSearch').click();
-        const options1Element = document.querySelector('#pairProductModal.open #goodsContent tbody > tr.content > td > a');
-        if (options1Element) {
-            options1Element.click();
-            document.querySelector('#confirmChangeRelationModal [name="changeRelation"][value="1"]').click();
-        }
+        setTimeout(() => {
+            const options1Element = document.querySelector('#pairProductModal.open #goodsContent tbody > tr.content > td > a');
+            if (options1Element) {
+                options1Element.click();
+                document.querySelector('#confirmChangeRelationModal [name="changeRelation"][value="1"]').click();
+            }
+        }, 300);
     }
 
     function orderMate(vid){
